766c1433034626759e610ef9ee1a94e80aa775b4
yashbim <ybimsara03@gmail.com>
2025-01-18 02:20:27 +0530

    Constants and avaialbility array added

--- src/main/java/org/example/FloorBathroom.java
+++ src/main/java/org/example/FloorBathroom.java
@@ -4,7 +4,11 @@ import java.util.concurrent.Semaphore;
 
 public class FloorBathroom {
 
+    final static int BATHROOM_STALLS = 6;
+    final static int NUM_EMPLOYEES = 6;
     Semaphore bathroomStalls = new Semaphore(6);
+    boolean [] stallBooker ;
+
 
     public static void main(String[] args) {
         System.out.println("Floor Bathroom Booking System");
