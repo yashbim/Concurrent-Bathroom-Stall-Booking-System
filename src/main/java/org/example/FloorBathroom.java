4cbf21c65a5a607fabe610b155c914aa539c1829
yashbim <ybimsara03@gmail.com>
2025-01-18 02:15:58 +0530

    Added semaphore

--- src/main/java/org/example/FloorBathroom.java
+++ src/main/java/org/example/FloorBathroom.java
@@ -1,7 +1,15 @@
 package org.example;
 
+import java.util.concurrent.Semaphore;
+
 public class FloorBathroom {
+
+    Semaphore bathroomStalls = new Semaphore(6);
+
     public static void main(String[] args) {
-        System.out.println("Floor Bathroom");
+        System.out.println("Floor Bathroom Booking System");
+
+
+
     }
 }
